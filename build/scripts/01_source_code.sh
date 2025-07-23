58f0527960337a37853b1691940fbca70b7c81dc
robinquca <155705374+robinquca@users.noreply.github.com>
2025-07-23 11:49:31 +0800

    Create 01_source_code.sh

---
+++ build/scripts/01_source_code.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ -n "$MODEL" ] && [ "$MODEL" == "r4se" ]; then 
+    git clone https://github.com/DHDAXCW/lede-rockchip openwrt;
+fi
+
+if [ -n "$MODEL" ] && [ "$MODEL" == "r5c" ]; then 
+    git clone https://github.com/DHDAXCW/lede-rockchip openwrt;
+fi
+
+if [ -n "$MODEL" ] && [ "$MODEL" == "x86-64" ]; then 
+    git clone https://github.com/coolsnowwolf/lede openwrt;
+fi
+
+cd openwrt
+rm -rf .git
+
+OPENWRTROOT=$(pwd)
+export OPENWRTROOT
+
+mkdir customfeeds
+git clone --depth=1 https://github.com/coolsnowwolf/packages customfeeds/packages
+git clone --depth=1 https://github.com/coolsnowwolf/luci customfeeds/luci
