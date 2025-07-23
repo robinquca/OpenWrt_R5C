57e07d5800576859ceb2d0a6085c7600f9376abb
robinquca <155705374+robinquca@users.noreply.github.com>
2025-07-23 11:56:27 +0800

    Create 04_community_package.sh

---
+++ build/scripts/04_community_package.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+OPENWRTROOT=$(pwd)
+export OPENWRTROOT
+
+# Clone community packages
+mkdir package/community
+pushd package/community
+
+# Add luci-app-xunlei
+svn co https://github.com/gngpp/nas-xunlei/trunk/openwrt
+
+# Add luci-app-alist
+git clone --depth=1 https://github.com/sbwml/luci-app-alist
+
+# Add Lienol's Packages
+git clone --depth=1 https://github.com/Lienol/openwrt-package
+
+# Add luci-app-passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall
+git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2
+svn export https://github.com/xiaorouji/openwrt-passwall/branches/luci/luci-app-passwall
+
+# Add luci-app-ssr-plus
+git clone --depth=1 https://github.com/fw876/helloworld
+
+# Add luci-app-vssr <M>
+git clone --depth=1 https://github.com/jerrykuku/lua-maxminddb.git
+git clone --depth=1 https://github.com/jerrykuku/luci-app-vssr 
+
+# Add luci-proto-minieap
+git clone --depth=1 https://github.com/ysc3839/luci-proto-minieap
+
+# Add OpenClash
+svn export https://github.com/vernesong/OpenClash/trunk/luci-app-openclash
+
+# Add ServerChan
+git clone --depth=1 https://github.com/tty228/luci-app-serverchan
+
+# Add luci-app-autotimeset
+git clone --depth=1 https://github.com/sirpdboy/luci-app-autotimeset
+
+# Add subconverter
+git clone --depth=1 https://github.com/tindy2013/openwrt-subconverter
+
+# Add luci-app-smartdns & smartdns
+svn export https://github.com/gngpp/openwrt-packages/trunk/luci-app-smartdns
+
+# Add luci-app-services-wolplus
+svn export https://github.com/msylgj/OpenWrt_luci-app/trunk/luci-app-services-wolplus
+
+# Add luci-app-poweroff
+git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff
+
+# Add OpenAppFilter
+git clone --depth=1 https://github.com/destan19/OpenAppFilter
+popd
+
+# OpenClash GEOIP
+mkdir -p files/etc/openclash
+GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
+GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
+wget -qO- $GEOIP_URL > files/etc/openclash/GeoIP.dat
+wget -qO- $GEOSITE_URL > files/etc/openclash/GeoSite.dat
